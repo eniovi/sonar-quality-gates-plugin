0d6c9d6bb6432001cb301ed27c847508bf3e7401
eniovi <enio.vi.santos@gmail.com>
2021-12-14 23:39:03 -0300

    feat(Sonar8x): add new verification to return SonarHttpRequester instance

--- src/main/java/org/quality/gates/sonar/api/SonarHttpRequesterFactory.java
+++ src/main/java/org/quality/gates/sonar/api/SonarHttpRequesterFactory.java
@@ -10,6 +10,7 @@ import org.quality.gates.jenkins.plugin.GlobalConfigDataForSonarInstance;
 import org.quality.gates.sonar.api5x.SonarHttpRequester5x;
 import org.quality.gates.sonar.api60.SonarHttpRequester60;
 import org.quality.gates.sonar.api61.SonarHttpRequester61;
+import org.quality.gates.sonar.api8x.SonarHttpRequester8x;
 
 import java.io.IOException;
 
@@ -21,7 +22,10 @@ class SonarHttpRequesterFactory {
 
     private static final String SONAR_API_SERVER_VERSION = "/api/server/version";
 
-    static SonarHttpRequester getSonarHttpRequester(GlobalConfigDataForSonarInstance globalConfigDataForSonarInstance) {
+    private SonarHttpRequesterFactory () {
+    }
+
+    static SonarHttpRequester getSonarHttpRequester (GlobalConfigDataForSonarInstance globalConfigDataForSonarInstance) {
 
         try {
             HttpGet request = new HttpGet(getSonarApiServerVersion(globalConfigDataForSonarInstance));
@@ -31,31 +35,37 @@ class SonarHttpRequesterFactory {
             CloseableHttpResponse response = client.execute(request, context);
             String sonarVersion = EntityUtils.toString(response.getEntity());
 
-            if (majorSonarVersion(sonarVersion) <= 5) {
-                return new SonarHttpRequester5x();
-            } else if (majorSonarVersion(sonarVersion) >= 6 && minorSonarVersion(sonarVersion) == 0) {
-                return new SonarHttpRequester60();
-            } else if (majorSonarVersion(sonarVersion) >= 6 && minorSonarVersion(sonarVersion) >= 1) {
-                return new SonarHttpRequester61();
-            } else {
-                throw new UnsuportedVersionException("Plugin doesn't suport this version of sonar api! Please contact the developer.");
-            }
+            return getSonarHttpRequesterInstance(sonarVersion);
         } catch (IOException e) {
             throw new ApiConnectionException(e.getLocalizedMessage());
         }
     }
 
-    private static String getSonarApiServerVersion(GlobalConfigDataForSonarInstance globalConfigDataForSonarInstance) {
+    private static String getSonarApiServerVersion (GlobalConfigDataForSonarInstance globalConfigDataForSonarInstance) {
 
         return globalConfigDataForSonarInstance.getSonarUrl() + SONAR_API_SERVER_VERSION;
     }
 
-    private static int majorSonarVersion(String sonarVersion) {
+    protected static SonarHttpRequester getSonarHttpRequesterInstance (String sonarVersion) {
+        if (majorSonarVersion(sonarVersion) <= 5) {
+            return new SonarHttpRequester5x();
+        } else if (majorSonarVersion(sonarVersion) >= 6 && minorSonarVersion(sonarVersion) == 0) {
+            return new SonarHttpRequester60();
+        } else if (majorSonarVersion(sonarVersion) >= 6 && majorSonarVersion(sonarVersion) < 8 && minorSonarVersion(sonarVersion) >= 1) {
+            return new SonarHttpRequester61();
+        } else if (majorSonarVersion(sonarVersion) >= 8) {
+            return new SonarHttpRequester8x();
+        } else {
+            throw new UnsuportedVersionException("Plugin doesn't suport this version of sonar api! Please contact the developer.");
+        }
+    }
+
+    private static int majorSonarVersion (String sonarVersion) {
 
         return Integer.parseInt(sonarVersion.split("\\.")[0]);
     }
 
-    private static int minorSonarVersion(String sonarVersion) {
+    private static int minorSonarVersion (String sonarVersion) {
 
         return Integer.parseInt(sonarVersion.split("\\.")[1]);
     }
